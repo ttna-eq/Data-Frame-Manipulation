c639ffc324d718f8e98e8a80375a6df97570c55c
ttna-eq <90914534+ttna-eq@users.noreply.github.com>
2021-12-07 21:09:01 +0700

    Update and rename 01_Read CSV.c to 01_read CSV.c

--- 01_read CSV.c
+++ 01_read CSV.c
@@ -2,12 +2,19 @@
 
 int main()
 {
+    // To input the file name in case that we want to read another file
+    
+    /* 
     const int file_size = 50;
-
     char file_name[file_size];
     printf("Please enter the location of .csv file: ");
     gets(file_name);
+    */
+    
+    // in this case file_name is "test1.csv"
 
+    char file_name[] = "test1.csv";
+    
     FILE* in_file = fopen(file_name,"r");
     if (in_file == NULL)
     {
