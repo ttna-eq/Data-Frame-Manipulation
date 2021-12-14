ea83ca1bb1859e7b30e7b9c79482b6fdfac1ef10
ttna-eq <90914534+ttna-eq@users.noreply.github.com>
2021-12-14 21:30:13 +0700

    Upload the new read_csv .c and .h

---
+++ read_csv.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int read_csv(char filename[])
+{
+    FILE* fp = fopen(filename, "r");
+
+    if(fp == NULL)
+    {
+        perror("Cannot open flie.");
+        exit(1);
+    }
+
+    char line[100];
+
+    while(fgets(line, 100, fp))
+    {
+        char* tk;
+
+        tk = strtok(line,",");
+        while(tk != NULL){
+            printf("%-10s",tk);
+            tk = strtok(NULL, ",");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ read_csv.h
@@ -0,0 +1,6 @@
+#ifndef READ_CSV_H_INCLUDED
+#define READ_CSV_H_INCLUDED
+
+int read_csv(char filename[]);
+
+#endif // READ_CSV_H_INCLUDED
