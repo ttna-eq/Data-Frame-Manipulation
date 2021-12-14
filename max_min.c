8b11cb2ffcd174a10311c3fca6eefbacc840fb8e
ttna-eq <90914534+ttna-eq@users.noreply.github.com>
2021-12-14 23:21:28 +0700

    Add functions to find max or min

---
+++ max_min.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "count_row_column.h"
+
+double max_func(char filename[], int column)
+{
+    FILE* fp = fopen(filename, "r");
+    char line[100];
+    double val, max;
+    int count_comma, buff, line_no = 0;
+    char val_str[20];
+
+    while( fgets(line, 100, fp) != NULL )
+        {
+            count_comma = 0;
+            buff = 0;
+
+            for(int i = 0; i < strlen(line); i++)
+            {
+                if(line[i] == ',')  // count how many comma read
+                    count_comma++;
+                else if(column == count_comma) // get string for the value from the specific column
+                {
+                    val_str[buff] = line[i];
+                    buff++;
+                }
+
+            }
+            if (line_no == 1) // assign first value to max
+                sscanf(val_str, "%lf", &max); // convert str into double
+
+
+            else if (line_no > 1) // compare the value between max and val
+            {
+                sscanf(val_str, "%lf", &val); // convert str into double
+
+                if(val > max)
+                    max = val;
+            }
+
+            line_no++;
+
+        }
+
+    fclose(fp);
+
+    return max;
+}
+
+double min_func(char filename[], int column)
+{
+    FILE* fp = fopen(filename, "r");
+    char line[100];
+    double val, max;
+    int count_comma, buff, line_no = 0;
+    char val_str[20];
+
+    while( fgets(line, 100, fp) != NULL )
+        {
+            count_comma = 0;
+            buff = 0;
+
+            for(int i = 0; i < strlen(line); i++)
+            {
+                if(line[i] == ',')  // count how many comma read
+                    count_comma++;
+                else if(column == count_comma) // get string for the value from the specific column
+                {
+                    val_str[buff] = line[i];
+                    buff++;
+                }
+
+            }
+            if (line_no == 1) // assign first value to max
+                sscanf(val_str, "%lf", &max); // convert str into double
+
+
+            else if (line_no > 1) // compare the value between max and val
+            {
+                sscanf(val_str, "%lf", &val); // convert str into double
+
+                if(val < max)
+                    max = val;
+            }
+
+            line_no++;
+
+        }
+
+    fclose(fp);
+
+    return max;
+}
---
+++ max_min.h
@@ -0,0 +1,7 @@
+#ifndef MAX_MIN_H_INCLUDED
+#define MAX_MIN_H_INCLUDED
+
+double max_func(char filename[], int column);
+double min_func(char filename[], int column);
+
+#endif // MAX_MIN_H_INCLUDED
