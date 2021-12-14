27ceeb744185c300cd6bce54236e8c2ae12ccdb0
ttna-eq <90914534+ttna-eq@users.noreply.github.com>
2021-12-14 21:20:35 +0700

    fix argument type

--- count_row_column.h
+++ count_row_column.h
@@ -1,7 +1,7 @@
 #ifndef COUNT_ROW_COLUMN_H_INCLUDED
 #define COUNT_ROW_COLUMN_H_INCLUDED
 
-int count_column(FILE* fp);
-int count_row(FILE* fp);
+int count_column(char filename[]);
+int count_row(char filename[]);
 
 #endif // COUNT_ROW_COLUMN_H_INCLUDED
